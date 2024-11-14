c95898f32db88874baeb2944c5af31c0ed73116a
Parth <parthpanchal133@gmail.com>
2024-11-14 23:39:20 +0530

    Service detail page updated

--- service-detail.php
+++ service-detail.php
@@ -134,8 +134,290 @@ include('template-parts/clients-slider.php');
         </div>
     </div>
 </section>
-<section class="services-cta">
+<section class="cta-sec">
+        <div class="container">
+            <div class="cta-wrapper">
+                <div class="cta-content-col">
+                    <h2>Ready To Revolutionize Your Business Landscape With Data Science?</h2>
+                    <p>Abode Enterprise is your ideal data science consulting company with a dedicated team of data
+                        scientists who will help you define world-class data solutions, solving your challenges.</p>
+                    <a href="#" class="btn">Consult Now <span class="btn-arrow"><i
+                                class="fas fa-arrow-right"></i></span></a>
+                </div>
+                <div class="cta-img-col">
+                    <div class="img-col-wrap">
+                        <div class="cta-img-wrap">
+                            <div class="cta-img">
+                                <img src="assets/images/man-with-phone.webp" alt="cta image">
+                            </div>
+                        </div>
+                        <div class="cta-img-content">
+                            <div class="single-capsule">
+                                <div class="small-icon">
+                                    <img src="assets/images/nda.svg" alt="nda">
+                                </div>
+                                <p>100% NDA-protected contract</p>
+                            </div>
+                            <div class="single-capsule">
+                                <div class="small-icon">
+                                    <img src="assets/images/resource.svg" alt="resource">
+                                </div>
+                                <p>100% resource substitution*</p>
+                            </div>
+                            <div class="single-capsule">
+                                <div class="small-icon">
+                                    <img src="assets/images/hiring-models.svg" alt="hiring models">
+                                </div>
+                                <p>Flexible hiring models</p>
+                            </div>
+                            <div class="single-capsule">
+                                <div class="small-icon">
+                                    <img src="assets/images/refund-policy.svg" alt="refund">
+                                </div>
+                                <p>100% refund policy*</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="case-study">
+        <div class="container">
+            <h2 class="section-title text-center">Case Study</h2>
+            <div class="case-study-wrap">
+                <div class="case-study-img-col">
+                    <div class="case-study-img">
+                        <img src="assets/images/case-study.jpg" alt="case study">
+                    </div>
+                </div>
+                <div class="case-study-content-col">
+                    <h3>Accurate customer profiling & enrichment for video communication company</h3>
+                    <p>customer profiles with appropriate segmentation helped the video communication company market
+                        their services effectively along with targeted upsell and cross sell.>Accurate customer
+                        profiling & enrichment for video communication company</p>
 
+                    <div class="case-study-action">
+                        <p>Video Communication Company, USA</p>
+                        <a href="#" class="btn btn-dark">Read Case Study <span class="btn-arrow"><i
+                                    class="fas fa-arrow-right"></i></span></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="benefits-sec">
+        <div class="container">
+            <h2 class="section-title text-center">Benefits</h2>
+            <p class="section-desc text-center">Using data science can be tough because of scattered and messy data, a shortage of data science expertise, and the overwhelming choices of tools, practices, and standards required for training and deploying models. Utilise Abode Enterprise’s Data Scientists’ to over come this problem.</p>
+            <div class="benefits-wrap">
+                <div class="single-benfit-block">
+                    <h4 class="single-benefit-title">Data Strategy Consulting</h4>
+                    <p>Every business is unique, and so is their Problem statement for data science solutions. Our expert data scientists take a custom approach to understand your unique needs and provide a solution to your unique business challenges, ensuring that you derive maximum value from your data.</p>
+                </div>
+                <div class="single-benfit-block">
+                    <h4 class="single-benefit-title">Data Collection and Preprocessing</h4>
+                    <p>At Abode Enterprise, we utilize tools and technologies including web scraping, cloud data collection platforms, and Web APIs to gather data tailored to the unique challenges for your business. Using our experience working with MongoDB, PostgreSQL, MySQL etc we make sure your data is clean, relevant, and ready for analysis.</p>
+                </div>
+                <div class="single-benfit-block">
+                    <h4 class="single-benefit-title">Predictive Analytics and Machine Learning</h4>
+                    <p>At Abode Enterprise, our data experts use latest tools like Python, Scikit-learn, TensorFlow, and PyTorch to turn your old data into meaningful insights. which helps you make better data driven decisions and understand the patterns about customer journey or issues in your production pipeline.</p>
+                </div>
+                <div class="single-benfit-block">
+                    <h4 class="single-benefit-title">Data Visualization and Business Intelligence</h4>
+                    <p>We understand how important dashboards are for businesses today. We use advanced tools like Tableau, Power BI, and Google Data Studio etc to make complex data easy to understand & mix beautiful visuals with practicality, creating dashboards that not only look great but also help with making better business decisions quickly.</p>
+                </div>
+                <div class="single-benfit-block">
+                    <h4 class="single-benefit-title">Natural Language Processing (NLP)</h4>
+                    <p>From sentiment analysis and text classification, chatbots and language translation or text to speech solution we utilise the power of NLTK, spaCy, and BERT etc to help businesses understand customer feedback, automate customer service, and gain insights and convert complex information to a natural language for everyone to understand.</p>
+                </div>
+                <div class="single-benfit-block">
+                    <h4 class="single-benefit-title">Big Data Solutions</h4>
+                    <p>Abode Enterprise Data Scientist work with tools like Hadoop, Spark, and NoSQL etc to create custom solutions that make huge amounts of data useful for improving customer service, streamlining operations, and finding new ways to make informed decisions while utilising our expertise.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="delivery-process">
+        <div class="container">
+            <h2 class="section-title text-center">Our Delivery Process</h2>
+
+            <div class="delivery-process-wrap">
+                <div class="single-process">
+                    <div class="single-process-inner text-center">
+                        <h4>Requirement Analysis</h4>
+                        <p>We offer in-depth analysis of client requirements and project objectives.</p>
+                    </div>
+                </div>
+                <div class="single-process">
+                    <div class="single-process-inner text-center">
+                        <h4>Data Exploration</h4>
+                        <p>Our experts meticulously explore and prepare data for AI model development.</p>
+                    </div>
+                </div>
+                <div class="single-process">
+                    <div class="single-process-inner text-center">
+                        <h4>Solution Designing</h4>
+                        <p>Our expert team then builds a tailored AI solution design that aligns with client needs.</p>
+                    </div>
+                </div>
+                <div class="single-process">
+                    <div class="single-process-inner text-center">
+                        <h4>AI Model Development</h4>
+                        <p>Our team builds and refines AI models using cutting-edge techniques.</p>
+                    </div>
+                </div>
+                <div class="single-process">
+                    <div class="single-process-inner text-center">
+                        <h4>Integration & Deployment</h4>
+                        <p>We integrate AI solutions into client systems and ensure smooth deployment.</p>
+                    </div>
+                </div>
+                <div class="single-process">
+                    <div class="single-process-inner text-center">
+                        <h4>Maintenance & Support</h4>
+                        <p>We provide ongoing support and updates, ensuring optimal performance post-deployment.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="why-choose">
+        <div class="container">
+            <h2 class="section-title text-center">Why Choose Us</h2>
+
+            <div class="why-choose-wrap">
+                <div class="single-choose-block">
+                    <div class="single-choose-img">
+                        <img src="assets/images/icon-one.svg" alt="icon">
+                    </div>
+                    <div class="single-choose-meta">
+                        <h3>Immediate Return on Investment</h3>
+                        <p>Reduced operational costs by 40% (up to 70% if your creation framework is required 24×7).</p>
+                    </div>
+                </div>
+                <div class="single-choose-block">
+                    <div class="single-choose-img">
+                        <img src="assets/images/icon-two.svg" alt="icon">
+                    </div>
+                    <div class="single-choose-meta">
+                        <h3>Minimise Errors and Loss of Revenue</h3>
+                        <p>De-hazard your innovation investment with demonstrated mastery of SQL Server DBA support groups.</p>
+                    </div>
+                </div>
+                <div class="single-choose-block">
+                    <div class="single-choose-img">
+                        <img src="assets/images/icon-three.svg" alt="icon">
+                    </div>
+                    <div class="single-choose-meta">
+                        <h3>Increase Business Agility</h3>
+                        <p>Outsource management of day-to-day system admin to be free to concentrate on new projects.</p>
+                    </div>
+                </div>
+                <div class="single-choose-block">
+                    <div class="single-choose-img">
+                        <img src="assets/images/icon-four.svg" alt="icon">
+                    </div>
+                    <div class="single-choose-meta">
+                        <h3>Improve Efficiencies and Uptime</h3>
+                        <p>Deploy experience in clustering and mirroring technologies for outstanding resilience and uptime</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="home-page-testimonials">
+    <div class="container">
+        <h2 class="section-title text-center">What Our Customers are Saying</h2>
+
+        <div class="home-page-testimonial-list">
+
+            <div class="single-testimonial-slide">
+                <div class="testimonial-slider-inner">
+                    <div class="testimonial-img">
+                        <img src="assets/images/user-one.webp" alt="user">
+                    </div>
+                    <div class="testimonial-name">
+                        <h3>Kevin Peterson</h3>
+                        <p class="designation">CEO, ABC Company</p>
+                    </div>
+                    <div class="quote-img">
+                        <i class="fas fa-quote-left"></i>
+                    </div>
+                    <p class="main-testimonial">
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veritatis delectus similique praesentium consectetur sit excepturi aperiam minus optio iure voluptatem.
+                    </p>
+                    <div class="social-connect">
+                        <a href="https://linkedin.com"><i class="fab fa-linkedin"></i></a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="single-testimonial-slide">
+                <div class="testimonial-slider-inner">
+                    <div class="testimonial-img">
+                        <img src="assets/images/user-two.webp" alt="user">
+                    </div>
+                    <div class="testimonial-name">
+                        <h3>Robert Dawid</h3>
+                        <p class="designation">Founder, My Company</p>
+                    </div>
+                    <div class="quote-img">
+                        <i class="fas fa-quote-left"></i>
+                    </div>
+                    <p class="main-testimonial">
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veritatis delectus similique praesentium consectetur sit excepturi aperiam minus optio iure voluptatem.
+                    </p>
+                    <div class="social-connect">
+                        <a href="https://linkedin.com"><i class="fab fa-linkedin"></i></a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="single-testimonial-slide">
+                <div class="testimonial-slider-inner">
+                    <div class="testimonial-img">
+                        <img src="assets/images/user-three.webp" alt="user">
+                    </div>
+                    <div class="testimonial-name">
+                        <h3>Malan C.</h3>
+                        <p class="designation">COO, XYZ Company</p>
+                    </div>
+                    <div class="quote-img">
+                        <i class="fas fa-quote-left"></i>
+                    </div>
+                    <p class="main-testimonial">
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veritatis delectus similique praesentium consectetur sit excepturi aperiam minus optio iure voluptatem.
+                    </p>
+                    <div class="social-connect">
+                        <a href="https://linkedin.com"><i class="fab fa-linkedin"></i></a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="single-testimonial-slide">
+                <div class="testimonial-slider-inner">
+                    <div class="testimonial-img">
+                        <img src="assets/images/user-two.webp" alt="user">
+                    </div>
+                    <div class="testimonial-name">
+                        <h3>Peter J.</h3>
+                        <p class="designation">Founder, MNC Company</p>
+                    </div>
+                    <div class="quote-img">
+                        <i class="fas fa-quote-left"></i>
+                    </div>
+                    <p class="main-testimonial">
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veritatis delectus similique praesentium consectetur sit excepturi aperiam minus optio iure voluptatem.
+                    </p>
+                    <div class="social-connect">
+                        <a href="https://linkedin.com"><i class="fab fa-linkedin"></i></a>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
 </section>
 <?php
 include('footer.php');
